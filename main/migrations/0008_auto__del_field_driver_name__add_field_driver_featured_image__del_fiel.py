3445eaf633e2de594c849f792823e15d51dd98ac
davit-gh <davsmile@yahoo.com>
2015-12-14 22:43:47 +0400

    add db models for html table in base.html

---
+++ main/migrations/0008_auto__del_field_driver_name__add_field_driver_featured_image__del_fiel.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Driver.name'
+        db.delete_column(u'main_driver', 'name')
+
+        # Adding field 'Driver.featured_image'
+        db.add_column(u'main_driver', 'featured_image',
+                      self.gf('mezzanine.core.fields.FileField')(max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+        # Deleting field 'Ride.featured_image'
+        db.delete_column(u'main_ride', 'featured_image')
+
+
+    def backwards(self, orm):
+        # Adding field 'Driver.name'
+        db.add_column(u'main_driver', 'name',
+                      self.gf('django.db.models.fields.CharField')(default=1, max_length=100),
+                      keep_default=False)
+
+        # Deleting field 'Driver.featured_image'
+        db.delete_column(u'main_driver', 'featured_image')
+
+        # Adding field 'Ride.featured_image'
+        db.add_column(u'main_ride', 'featured_image',
+                      self.gf('mezzanine.core.fields.FileField')(max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+
+    models = {
+        u'main.city': {
+            'Meta': {'object_name': 'City'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_hy': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'main.driver': {
+            'Meta': {'ordering': "(u'_order',)", 'object_name': 'Driver', '_ormbases': [u'pages.Page']},
+            'content': ('mezzanine.core.fields.RichTextField', [], {}),
+            'featured_image': ('mezzanine.core.fields.FileField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            u'page_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['pages.Page']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        u'main.homepage': {
+            'Meta': {'ordering': "(u'_order',)", 'object_name': 'HomePage', '_ormbases': [u'pages.Page']},
+            'content': ('mezzanine.core.fields.RichTextField', [], {}),
+            'content_heading': ('django.db.models.fields.CharField', [], {'default': "'About us!'", 'max_length': '200'}),
+            'featured_portfolio': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['main.Portfolio']", 'null': 'True', 'blank': 'True'}),
+            u'page_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['pages.Page']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        u'main.portfolio': {
+            'Meta': {'ordering': "(u'_order',)", 'object_name': 'Portfolio', '_ormbases': [u'pages.Page']},
+            'columns': ('django.db.models.fields.CharField', [], {'default': "'3'", 'max_length': '1'}),
+            'content': ('mezzanine.core.fields.RichTextField', [], {'blank': 'True'}),
+            u'page_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['pages.Page']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        u'main.portfolioitem': {
+            'Meta': {'ordering': "(u'_order',)", 'object_name': 'PortfolioItem', '_ormbases': [u'pages.Page']},
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'portfolioitems'", 'blank': 'True', 'to': u"orm['main.PortfolioItemCategory']"}),
+            'content': ('mezzanine.core.fields.RichTextField', [], {}),
+            'featured_image': ('mezzanine.core.fields.FileField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'href': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            u'page_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['pages.Page']", 'unique': 'True', 'primary_key': 'True'}),
+            'short_description': ('mezzanine.core.fields.RichTextField', [], {'blank': 'True'})
+        },
+        u'main.portfolioitemcategory': {
+            'Meta': {'ordering': "('title',)", 'object_name': 'PortfolioItemCategory'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'})
+        },
+        u'main.portfolioitemimage': {
+            'Meta': {'ordering': "(u'_order',)", 'object_name': 'PortfolioItemImage'},
+            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'file': ('mezzanine.core.fields.FileField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'portfolioitem': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'images'", 'to': u"orm['main.PortfolioItem']"})
+        },
+        u'main.ride': {
+            'Meta': {'ordering': "(u'_order',)", 'object_name': 'Ride'},
+            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'driver': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'rides'", 'to': u"orm['main.Driver']"}),
+            'fromwhere': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'leavedate': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'towhere': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'main.ridesearch': {
+            'Meta': {'object_name': 'Ridesearch'},
+            'fromwhere': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'leavedate': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'passanger_number': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
+            'towhere': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'main.slide': {
+            'Meta': {'ordering': "(u'_order',)", 'object_name': 'Slide'},
+            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'homepage': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'slides'", 'to': u"orm['main.HomePage']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image_back': ('mezzanine.core.fields.FileField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'image_front': ('mezzanine.core.fields.FileField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '400', 'blank': 'True'})
+        },
+        u'pages.page': {
+            'Meta': {'ordering': "(u'titles',)", 'object_name': 'Page'},
+            '_meta_title': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'content_model': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'expiry_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'gen_description': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_menus': ('mezzanine.pages.fields.MenusField', [], {'default': '(1, 2, 3)', 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'in_sitemap': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            u'keywords_string': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'login_required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "u'children'", 'null': 'True', 'to': u"orm['pages.Page']"}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'short_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '2'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'titles': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'null': 'True'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'null': 'True'})
+        },
+        u'sites.site': {
+            'Meta': {'ordering': "(u'domain',)", 'object_name': 'Site', 'db_table': "u'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['main']
